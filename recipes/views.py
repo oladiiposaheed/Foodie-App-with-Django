bbe4ccf87a7e2e6656fe4fa06bf69f43d1a879f2
Saheed <oladiiposaheed@gmail.com>
2025-07-23 08:30:24 +0100

    Revert recipe category filter to show all recipes in the recipes view

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,8 @@ from recipes.models import Recipe
 
 # Create your views here.
 def recipes(request):
-    recipes_list = Recipe.objects.filter(category__name__icontains='West African Cuisine')
+    recipes_list = Recipe.objects.all()
+    #recipes_list = Recipe.objects.filter(category__name__icontains='West African Cuisine')
     dict = {'recipes': recipes_list}
     print(dict)
     return HttpResponse("This is the recipes page.")
