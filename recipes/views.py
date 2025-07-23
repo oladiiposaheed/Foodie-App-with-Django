10cbf351b8b50e0379743a2e325216ed8a52796b
Saheed <oladiiposaheed@gmail.com>
2025-07-23 08:27:09 +0100

    Fix recipe category filter to be case-insensitive in the recipes view

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from recipes.models import Recipe
 
 # Create your views here.
 def recipes(request):
-    recipes_list = Recipe.objects.filter(category__name__iexact='west African Cuisine')
+    recipes_list = Recipe.objects.filter(category__name__icontains='West African Cuisine')
     dict = {'recipes': recipes_list}
     print(dict)
     return HttpResponse("This is the recipes page.")
