7f2561fb49e9270c4a9a43531d24d89ada939b69
Saheed <oladiiposaheed@gmail.com>
2025-07-23 06:13:24 +0100

    Foodie App with Django

--- foodieApp/admin.py
+++ foodieApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from foodieApp.models import Category, Recipe
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'date')
+    search_fields = ('name',)
+    
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'date')
+    search_fields = ('name', 'category__name') # Enables searching by category name, category__name means we can search by the related field's name
+    
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Recipe, RecipeAdmin)  # Registering Recipe model without custom admin class
+
--- foodieApp/models.py
+++ foodieApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    #description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    ingredients = models.TextField()
+    directions = models.TextField()
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='recipes')
+    
+    def __str__(self):
+        return self.name
+    
+    
